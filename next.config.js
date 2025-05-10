8b11d1713fdce4479998475530262057a2f5cabb
Sladed49er <matt@everysolutionit.com>
2025-05-10 10:30:03 -0700

    next JS AGAIN

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   typescript: {
-    ignoreBuildErrors: true,    // ← turn off TS for prod builds
+    ignoreBuildErrors: true,
   },
   eslint: {
-    ignoreDuringBuilds: true,   // ← optional: don’t block on ESLint either
+    ignoreDuringBuilds: true,
   },
 };
